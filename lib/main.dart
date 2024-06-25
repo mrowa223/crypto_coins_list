89859189910ce93f5e5f22c5b9002f88cf46f96a
Kirill <kirillshermadov.cs@gmail.com>
2024-06-25 23:54:57 +0500

    add routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 void main() {
   runApp(const CryptoCurrenciesListApp());
@@ -13,61 +14,84 @@ class CryptoCurrenciesListApp extends StatelessWidget {
     return MaterialApp(
       title: 'Crypto Currencies List',
       theme: ThemeData(
-        scaffoldBackgroundColor: Colors.black,
-        primarySwatch: Colors.green,
+        scaffoldBackgroundColor: const Color.fromARGB(255, 31, 31, 31),
+        primarySwatch: Colors.yellow,
+        dividerColor: Colors.white24,
+        appBarTheme: const AppBarTheme(
+            backgroundColor: Color.fromARGB(255, 31, 31, 31),
+            titleTextStyle: TextStyle(
+                color: Colors.white,
+                fontSize: 20,
+                fontWeight: FontWeight.w700)),
+        listTileTheme: const ListTileThemeData(iconColor: Colors.white),
+        textTheme: TextTheme(
+            bodyMedium: const TextStyle(
+                color: Colors.white, fontWeight: FontWeight.w500, fontSize: 20),
+            labelSmall: TextStyle(
+                color: Colors.white.withOpacity(0.6),
+                fontWeight: FontWeight.w700,
+                fontSize: 14)),
         // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Crypto Currencies List'),
+      routes: {
+        '/': (context) => CryptoListScreen(),
+        '/coin': (context) => CryptoCoinScreen()
+      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class CryptoListScreen extends StatefulWidget {
+  const CryptoListScreen({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CryptoListScreen> createState() => _CryptoListScreenState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+class _CryptoListScreenState extends State<CryptoListScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: theme.appBarTheme.backgroundColor,
+        title: Text('CryptoCurrenciesList'),
+        leading: const Icon(Icons.arrow_back),
+      ),
+      body: ListView.separated(
+          itemCount: 10,
+          separatorBuilder: (context, index) => Divider(),
+          itemBuilder: (context, i) => ListTile(
+                leading: SvgPicture.asset(
+                  'assets/svg/bitcoin_logo.svg',
+                  height: 30,
+                  width: 30,
+                ),
+                title: Text(
+                  'Bitcoin',
+                  style: theme.textTheme.bodyMedium,
+                ),
+                subtitle: Text(
+                  '20000\$',
+                  style: theme.textTheme.labelSmall,
+                ),
+                trailing: Icon(Icons.arrow_forward_ios),
+                onTap: () {},
+              )),
+    );
   }
+}
+
+class CryptoCoinScreen extends StatelessWidget {
+  const CryptoCoinScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-        leading: Icon(Icons.arrow_back),
+        title: Text('Bitcoin'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
